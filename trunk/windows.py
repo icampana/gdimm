59fa76bf4bad26375f20803496e2a603b5f4ee26
ivan.campana <ivan.campana@4bc05c28-652a-0410-865c-31c25e1cfdef>
2007-03-09 02:10:00 +0000

    Correcciones de ventanas Agregados valores al svn para que no tome en cuenta los archivos de respaldo y bytecode de python

--- trunk/windows.py
+++ trunk/windows.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+#
+#      This program is free software; you can redistribute it and/or modify
+#      it under the terms of the GNU General Public License as published by
+#      the Free Software Foundation; either version 2 of the License, or
+#      (at your option) any later version.
+#
+#      This program is distributed in the hope that it will be useful,
+#      but WITHOUT ANY WARRANTY; without even the implied warranty of
+#      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#      GNU General Public License for more details.
+#
+#      You should have received a copy of the GNU General Public License
+#      along with this program; if not, write to the Free Software
+#      Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+
+import sys
 
 try:
     import pygtk
@@ -14,6 +31,10 @@ except:
 
 import configuration
 
+def gtk_main_quit():
+	gtk.main_quit
+	sys.exit(0)
+
 class wndBase:
 	windowname = ''
 	handlers = {}
@@ -27,8 +48,8 @@ class wndBase:
 			
 			#~ self.win.set_modal(True)
 		
-			if self.handlers:
-				self.wTree.signal_autoconnect(self.handlers)
+			#~ if self.handlers:
+			self.wTree.signal_autoconnect(self)
 
 	def init(self):
 		"Función virtual que debe ser sobreescrita para especificar los valores de arranque del formulario"
@@ -49,12 +70,23 @@ class wndBase:
 		while gtk.events_pending():
 			gtk.main_iteration()
 			
+	def set_modal(self, bool = False):
+		self.win.set_modal(bool)
+			
 	def set_name(self, name):
 		if name:
 			self.windowname = name
 			return True
 		else:
 			return False
+
+	def setParent(self, parent):
+		if parent:
+			self.back_page = parent
+			
+	def goto_home(self, widget, *args):
+		self.hide()
+	
 
 class wndContribuyentes(wndBase):
 
@@ -73,23 +105,37 @@ class wndContribuyentes(wndBase):
 class wndAbout(wndBase):
 	def init(self):
 		self.set_name("wndAcerca")
+		
+	def on_wndAcerca_close(self, *args):
+		self.back_page.show()
 
-class gDIMM(wndBase):
+class gDIMM:
+	
+	def __init__(self):
+		pass
+		
+	def start(self):
+		mainWindow = wndIntro()
+		mainWindow.show()
+		gtk.main()
+	
+class wndIntro(wndBase):
 	def init( self ):
 		self.set_name("wndIntro")
-		self.handlers = {"gtk_main_quit" : gtk.main_quit,
-			"on_btnAbout_clicked" : self.help_about,
-			"on_btnNuevaDeclaracion_clicked": self.nueva_declaracion,
-			}
+		#~ self.handlers = {"gtk_main_quit" : gtk.main_quit,
+			#~ }
 
-	def help_about(self, menuItem):
+	def on_btnAbout_clicked(self, *args):
 		vAcercaDe = wndAbout()
-		#~ vAcercaDe.set_modal(True)
-		self.hide()
+		vAcercaDe.setParent(self)
+		vAcercaDe.set_modal(True)
+		#~ self.hide()
 		vAcercaDe.show()
+	
+	def on_btnClose_clicked(self, *args):
+		gtk_main_quit()
 		
-		
-	def nueva_declaracion(self, *args):
+	def on_btnNuevaDeclaracion_clicked(self, *args):
 		vContribuyentes = wndContribuyentes()
 		self.hide()
 		vContribuyentes.show()
