aedfff86103ece33a50c1c3223e0bd0523171626
ivan.campana <ivan.campana@4bc05c28-652a-0410-865c-31c25e1cfdef>
2007-03-21 16:36:05 +0000

    agregado archivo de test con instrucciones para evaluar el engine y configuraciones de python incluida la ruta completa para el llamado del script de python

---
+++ test.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys, os
+print sys.path[0]
+
+#~ from data import *
+#~ c = Contribuyente()
+#~ c.set_ruc("0920327426001")
+#~ c.set_nombre("IVAN GABRIEL CAMPAÑA NARANJO")
+#~ c.set_tipo_documento("C")
+#~ c.set_documento("0920327426")
+#~ #print c
+
+#~ c2 = Contribuyente()
+#~ c2.set_ruc("0920188877001")
+#~ c2.set_nombre("DIANA VERONICA PATIÑO FLOR")
+#~ c2.set_tipo_documento("C")
+#~ c2.set_documento("0920188877")
+
+#~ c3 = Contribuyente()
+#~ c3.set_ruc("0920188874001")
+#~ c3.set_nombre("VERONICA FLOR")
+#~ c3.set_tipo_documento("P")
+#~ c3.set_documento("0920188874")
+
+#~ l = ListaContribuyentes()
+#~ l.add(c)
+#~ l.add(c2)
+#~ l.load()
+#~ l.add(c3)
+#~ l.save()
