c270b24fd82e558c7758b18fdd271186653aac0a
ivan.campana <ivan.campana@4bc05c28-652a-0410-865c-31c25e1cfdef>
2007-03-21 16:37:18 +0000

    eliminada informacion confidencial del archivo de pruebas

--- test.py
+++ test.py
@@ -12,12 +12,6 @@ print sys.path[0]
 #~ c.set_documento("0920327426")
 #~ #print c
 
-#~ c2 = Contribuyente()
-#~ c2.set_ruc("0920188877001")
-#~ c2.set_nombre("DIANA VERONICA PATIÑO FLOR")
-#~ c2.set_tipo_documento("C")
-#~ c2.set_documento("0920188877")
-
 #~ c3 = Contribuyente()
 #~ c3.set_ruc("0920188874001")
 #~ c3.set_nombre("VERONICA FLOR")
@@ -25,8 +19,7 @@ print sys.path[0]
 #~ c3.set_documento("0920188874")
 
 #~ l = ListaContribuyentes()
-#~ l.add(c)
-#~ l.add(c2)
 #~ l.load()
+#~ l.add(c)
 #~ l.add(c3)
 #~ l.save()
